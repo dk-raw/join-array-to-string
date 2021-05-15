3adc16b08ba9f1b8d3c077ea9a0ab7b0b60bf442
dk.raw <hello@dkatsios.tk>
2021-05-15 13:55:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JoinToString('D', 'O', 'G', 'E');
+        }
+        private static void JoinToString(char foo1, char foo2, char foo3, char foo4)
+        {
+            //First way
+            char[] p = new char[] { foo1, foo2, foo3, foo4 };
+            string final = new string(p);
+            Console.WriteLine(final);
+            Console.WriteLine("~~~~~~~~~~~~");
+            //Second way
+            string final2 = string.Join("", foo1, foo2, foo3, foo4);
+            Console.WriteLine(final2);
+        }
+    }
+}
